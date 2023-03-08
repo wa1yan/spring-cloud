b8964449464fd8d29bd06b68a87892f8869caea4
wa1yan <waiyanucsy1@gmail.com>
2023-03-08 10:28:16 +0630

    commit for service discovery with eureka

---
+++ service1/src/main/java/dev/waiyanhtet/service/Service1Application.java
@@ -0,0 +1,23 @@
+package dev.waiyanhtet.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Service1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from service1";
+	}
+
+}
---
+++ service1/src/test/java/dev/waiyanhtet/service/Service1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.waiyanhtet.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
